a7820d63fcc85b946cd1dc09d7e0e4773cd6dda0
Chandra Haldar <chandrahaldar329@gmail.com>
2026-05-16 13:46:49 +0530

    Made a product listing page in "Handmade Bliss"

---
+++ PLP.js
@@ -0,0 +1,20 @@
+let wishlistItems = [];
+
+// Login Modal Functions
+function openLoginModal() {
+    alert('Login functionality coming soon!');
+}
+
+// Wishlist Functions
+function openWishlist() {
+    alert('Wishlist functionality coming soon!');
+}
+
+// Navigation Functions
+function openSearch() {
+    alert('Profile functionality coming soon!');
+}
+
+function openCart() {
+    alert('Cart is empty');
+}
